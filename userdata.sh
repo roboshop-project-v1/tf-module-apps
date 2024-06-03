cc82ada4093a0ea80583eed783be2e72d064a892
jagadeesh6jaga <jagadeesh6jaga@gmail.com>
2024-06-03 21:33:02 +0530

    okok

--- userdata.sh
+++ userdata.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-
+sudo sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
+sudo sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
 yum install ansible -y &>>/opt/userdata.log
 ansible-pull -i localhost, -U https://github.com/roboshop-project-v1/roboshop-ansible.git main.yml -e component=${component} -e env = ${env} &>>/opt/userdata.log
