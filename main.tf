2ff54f19e02e1e45e43f344de7b8495081bcc6f1
jagadeesh6jaga <jagadeesh6jaga@gmail.com>
2024-06-05 23:44:09 +0530

    okok

--- main.tf
+++ main.tf
@@ -106,7 +106,7 @@ resource "aws_lb_listener_rule" "apps" {
 resource "aws_lb_target_group" "public" {
   count = var.component == "frontend"? 1:0
   name     = "${local.name_prefix}-public"
-  port     = var.port
+  port     = 80
   target_type = "ip"
   protocol = "HTTP"
   vpc_id   = var.default_vpc_id
